b941404026c43953ad86335760e8b46f455a1b10
himax82 <himax82@gmail.com>
2021-10-13 00:13:11 +0800

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -10,7 +10,7 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
             System.out.print("\r load: " + process[i]);
             if (i == process.length - 1) {
