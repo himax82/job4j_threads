0d09687de17029f3a267773b87ab3a9c15663b18
himax82 <himax82@gmail.com>
2021-10-13 13:26:29 +0800

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/concurrent/Cache.java
@@ -0,0 +1,12 @@
+package concurrent;
+
+public final class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
