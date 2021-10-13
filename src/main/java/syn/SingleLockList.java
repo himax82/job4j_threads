1eed60001daeb21f1a37228837d9e884d7605fb8
himax82 <himax82@gmail.com>
2021-10-13 22:37:36 +0800

    4. ThreadSafe динамический список [#1105]

---
+++ src/main/java/syn/SingleLockList.java
@@ -0,0 +1,38 @@
+package syn;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T>, Cloneable {
+
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = clone(list);
+    }
+
+    public synchronized void add(T value) {
+        T copy = value;
+        list.add(copy);
+    }
+
+    public synchronized T get(int index) {
+        T copy = list.get(index);
+        return copy;
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return clone(list).iterator();
+    }
+
+    private synchronized List<T> clone(List<T> list) {
+        return new ArrayList<>(list);
+    }
+}
