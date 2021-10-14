632625685fd68284aa591b5a86c650a1e72f7610
himax82 <himax82@gmail.com>
2021-10-15 00:10:40 +0800

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/pool/ThreadPool.java
@@ -0,0 +1,21 @@
+package pool;
+
+import wait.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    public void work(Runnable job) {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Thread t : threads) {
+            t.interrupt();
+        }
+    }
+}
