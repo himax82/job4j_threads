d802388ad52deb18e78da03c479c4fab7146ba9a
himax82 <himax82@gmail.com>
2021-10-14 13:38:56 +0800

    3. Класс хранилища пользователей UserStorage [#1104 #215579]

--- src/main/java/syn/User.java
+++ src/main/java/syn/User.java
@@ -1,13 +1,8 @@
 package syn;
 
-import net.jcip.annotations.GuardedBy;
-import net.jcip.annotations.ThreadSafe;
-
-@ThreadSafe
 public class User {
 
     private final int id;
-    @GuardedBy("this")
     private int amount;
 
     public User(int id, int amount) {
--- src/main/java/syn/UserStore.java
+++ src/main/java/syn/UserStore.java
@@ -4,53 +4,43 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStore {
 
     @GuardedBy("this")
-    private final List<User> users = new ArrayList<>();
+    private final Map<Integer, Integer> users = new HashMap<>();
 
     public synchronized boolean add(User user) {
-       return users.add(user);
+       users.put(user.getId(), user.getAmount());
+       return true;
     }
 
     public synchronized boolean update(User user) {
-        boolean result = false;
-        for (User u : users) {
-            if (u.getId() == user.getId()) {
-                u.setAmount(user.getAmount());
-                result = true;
-            }
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user.getAmount());
+            return true;
         }
-        return result;
+        return false;
     }
 
     public synchronized boolean delete(User user) {
-        return users.remove(user);
-    }
-
-    public synchronized User findFromId(int id) {
-        User user = null;
-        for (User r : users) {
-            if (r.getId() == id) {
-                user = r;
-            }
-        }
-        return user;
+        return users.remove(user.getId(), user.getAmount());
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        User from = findFromId(fromId);
-        User to = findFromId(toId);
-        if (from != null && to != null) {
-            from.setAmount(from.getAmount() - amount);
-            to.setAmount(to.getAmount() + amount);
-        } else {
-            return false;
+        int balance = users.get(fromId) - amount;
+        if (users.containsKey(fromId) && users.containsKey(toId)) {
+            if (balance > 0) {
+                users.put(fromId, balance);
+                users.put(toId, users.get(toId) + amount);
+                return true;
+            }
         }
-        return update(from) && update(to);
+        return false;
     }
 
 }
