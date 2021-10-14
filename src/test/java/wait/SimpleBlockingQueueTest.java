bcafec61ee5fe86d3593c18661da4b9086e7460c
himax82 <himax82@gmail.com>
2021-10-14 21:28:58 +0800

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/wait/SimpleBlockingQueueTest.java
+++ src/test/java/wait/SimpleBlockingQueueTest.java
@@ -1,9 +1,13 @@
 package wait;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -15,9 +19,9 @@ public class SimpleBlockingQueueTest {
         SimpleBlockingQueue<Integer> simple = new SimpleBlockingQueue<>();
         Thread threadOffer = new Thread(
                 () -> {
-                    simple.offer(1);
-                    simple.offer(2);
-                    simple.offer(3);
+                        simple.offer(1);
+                        simple.offer(2);
+                        simple.offer(3);
                 }
         );
         Thread threadPoll = new Thread(
@@ -37,4 +41,65 @@ public class SimpleBlockingQueueTest {
         assertThat(list, is(List.of(2, 3)));
     }
 
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.getQueue().isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+    @Test
+    public void whenFetchAllThenGetNoAll() throws InterruptedException {
+        final ArrayList<Integer> list = new ArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(5, 9).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!(queue.getQueue().size() == 1) || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            list.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(queue.poll(), is(8));
+    }
 }
