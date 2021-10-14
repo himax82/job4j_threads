59571031a6a3d99b49dea4558bebbbc4ace0c481
himax82 <himax82@gmail.com>
2021-10-14 14:53:09 +0800

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/wait/CountBarrier.java
@@ -0,0 +1,33 @@
+package wait;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+       total++;
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count != total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
