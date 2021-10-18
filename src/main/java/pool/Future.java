50eacdc7756ba3c68c26b99312d30448d6d05180
himax82 <himax82@gmail.com>
2021-10-18 20:05:57 +0800

    1. Реализовать ThreadPool [#1099 #216107]

---
+++ src/main/java/pool/Future.java
@@ -0,0 +1,118 @@
+package pool;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Future {
+
+    private static void iWork() throws InterruptedException {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Вы: Я работаю");
+            TimeUnit.SECONDS.sleep(1);
+            count++;
+        }
+    }
+
+    public static CompletableFuture<Void> goToTrash() {
+        return CompletableFuture.runAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел выносить мусор");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я вернулся!");
+                }
+        );
+    }
+
+    public static void runAsyncExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко");
+        iWork();
+        System.out.println("Куплено: " + bm.get());
+    }
+
+    public static void thenAcceptExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко");
+        bm.thenAccept((product) -> System.out.println("Сын: Я убрал " + product + " в холодильник "));
+        iWork();
+        System.out.println("Куплено: " + bm.get());
+    }
+
+    public static CompletableFuture<String> buyProduct(String product) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел в магазин");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я купил " + product);
+                    return product;
+                }
+        );
+    }
+
+    public static void thenRunExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        gtt.thenRun(() -> {
+            int count = 0;
+            while (count < 3) {
+                System.out.println("Сын: я мою руки");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+            System.out.println("Сын: Я помыл руки");
+        });
+        iWork();
+    }
+
+    public static void thenApplyExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко")
+                .thenApply((product) -> "Сын: я налил тебе в кружку " + product + ". Держи.");
+        iWork();
+        System.out.println(bm.get());
+    }
+
+    public static void thenComposeExample() throws Exception {
+        CompletableFuture<Void> result = buyProduct("Молоко").thenCompose(a -> goToTrash());
+        iWork();
+    }
+
+    public static void thenCombineExample() throws Exception {
+        CompletableFuture<String> result = buyProduct("Молоко")
+                .thenCombine(buyProduct("Хлеб"), (r1, r2) -> "Куплены " + r1 + " и " + r2);
+        iWork();
+        System.out.println(result.get());
+    }
+
+    public static CompletableFuture<Void> washHands(String name) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + ", моет руки");
+        });
+    }
+
+    public static void allOfExample() throws Exception {
+        CompletableFuture<Void> all = CompletableFuture.allOf(
+                washHands("Папа"), washHands("Мама"),
+                washHands("Ваня"), washHands("Боря")
+        );
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    public static void main(String[] args) throws Exception {
+        allOfExample();
+    }
+}
